51a94151aa228ef20dcc8c3039ca2597c1097d9c
FTB <fernandot371@gmail.com>
2021-03-30 19:15:02 -0600

    end sec 4, interfaces, methods, facory & singleton patters

--- Hydra/webserver.go
+++ Hydra/webserver.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"GoMastering/Hydra/hlogger"
 	"fmt"
 	"net/http"
 )
@@ -11,12 +12,20 @@ import (
 // HandleFunc
 // ListenAndServe
 
+//
+// loogger with singleton pattern
+
 func main() {
-	fmt.Println("web server hydra example")
+	logger := *hlogger.GetInstance()
+	logger.Println("Starting hydra web service")
+
 	http.HandleFunc("/", sroot)
 	http.ListenAndServe(":8080", nil)
 }
 
 func sroot(w http.ResponseWriter, r *http.Request) {
+	logger := *hlogger.GetInstance()
 	fmt.Fprintf(w, "Welcome to the hydra software system")
+
+	logger.Println("Received an http request on root url")
 }
