e74c1bac897ce41014b8ab13f2ec19b5e6802a89
Fernando Torres <fernandot371@gmail.com>
2021-04-02 18:05:40 -0600

    test sec 6

---
+++ Hydra/hydradblayer/hydradblayer_test.go
@@ -0,0 +1,17 @@
+package hydradblayer
+
+/*import "math/rand"
+
+
+func BenchmarkSQLDBReads(b *testing.B) {
+	dblayer, err := ConnectionDabatase("mysql", "gouser:gouser@/Hydra")
+	if err != nil {
+		b.Fatal("Could not connect to Hydra chat system: ", err)
+	}
+	findMembersMB(b, dblayer)
+}
+
+func findMembersMB(b *testing.B, dblayer DBLayer) {
+	rand.Seed(time.Now().UnixNano())
+
+}*/
